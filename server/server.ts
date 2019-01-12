901e56178e836e232cc046c6a757579ea6151abf
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-01-12 17:36:14 -0200

    Configurando para deploy

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import App from './app';
 
-const port: number = Number(process.env.port) || 8080;
+const port: number = Number(process.env.PORT) || 8080;
 
 App.app.listen(port, () => console.log('Server on'));
